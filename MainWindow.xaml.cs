89b52ae808830aeb04fadc74f470178a58f1c961
jeyderht <jeyderhtoficial@gmail.com>
2022-03-26 15:39:52 -0500

    se agrega el Proyecto

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFUIKitProfessional.Themes;
+using WPFUIKitProfessional.Pages;
+
+namespace WPFUIKitProfessional
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Themes_Click(object sender, RoutedEventArgs e)
+        {
+            if (Themes.IsChecked == true)
+                ThemesController.SetTheme(ThemesController.ThemeTypes.Dark);
+            else
+                ThemesController.SetTheme(ThemesController.ThemeTypes.Light);
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void rdHome_Click(object sender, RoutedEventArgs e)
+        {
+            frameContent.Navigate(new Home());
+        }
+
+        private void rdAnalytics_Click(object sender, RoutedEventArgs e)
+        {
+            frameContent.Navigate(new Analytics());
+        }
+
+        private void rdMessages_Click(object sender, RoutedEventArgs e)
+        {
+            frameContent.Navigate(new Messages());
+        }
+
+        private void rdCollections_Click(object sender, RoutedEventArgs e)
+        {
+            frameContent.Navigate(new Collections());
+        }
+
+        private void rdUsers_Click(object sender, RoutedEventArgs e)
+        {
+            frameContent.Navigate(new Users());
+        }
+    }
+}
---
+++ Themes/ThemesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFUIKitProfessional.Themes
+{
+    public static class ThemesController
+    {
+        public static ThemeTypes CurrentTheme { get; set; }
+
+        public enum ThemeTypes
+        {
+            Light, Dark
+        }
+
+        public static ResourceDictionary ThemeDictionary
+        {
+            get { return Application.Current.Resources.MergedDictionaries[0]; }
+            set { Application.Current.Resources.MergedDictionaries[0] = value; }
+        }
+
+        private static void ChangeTheme(Uri uri)
+        {
+            ThemeDictionary = new ResourceDictionary() { Source = uri };
+        }
+
+        public static void SetTheme(ThemeTypes theme)
+        {
+            string themeName = null;
+            CurrentTheme = theme;
+            switch (theme)
+            {
+                case ThemeTypes.Dark: themeName = "DarkTheme"; break;
+                case ThemeTypes.Light: themeName = "LightTheme"; break;
+            }
+
+            try
+            {
+                if (!string.IsNullOrEmpty(themeName))
+                    ChangeTheme(new Uri($"Themes/{themeName}.xaml", UriKind.Relative));
+            }
+            catch { }
+        }
+    }
+}
